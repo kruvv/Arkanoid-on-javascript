e01a4fbd6b13c711f45b3e683e3a1cf7c0c95575
kruvv <kruvv@mail.ru>
2018-12-27 18:46:50 +0300

    Очки и проигрыш. Управление шариком.

--- ball.js
+++ ball.js
@@ -1,6 +1,7 @@
 // Обьект шар
 var ball = {
-  speed: 1,
+  speedX: 1,
+  speedY: 1,
   dx: 1,
   dy: -1,
   color: 'blue',
@@ -20,8 +21,8 @@ var ball = {
   },
   // Движение шара
   move: function() {
-    this.x += this.speed * this.dx;
-    this.y += this.speed * this.dy;
+    this.x += this.speedX * this.dx;
+    this.y += this.speedY * this.dy;
   },
   // Обработка столкновения шара
   collision: function() {
@@ -31,22 +32,36 @@ var ball = {
         , enemy.x, enemy.y, enemy.width, enemy.height)) {
           this.dy *= -1;
           grid.destroy(i);
-          this.speed += 0.1;
+          this.speedY += 0.1;
           player.speed += 0.1;
+          player.updScore(1);
       }
     }
     if(isCollision(this.x - this.radius, this.y - this.radius, this.radius*2, this.radius*2
       , player.x, player.y, player.width, player.height)) {
-        this.dy *= -1;
+        this.dy = -1;
+        if (this.dx == player.dx) {
+          this.speedX *= 1.2;
+        }else {
+          this.speedX /= player.dx != 0 ? 1.2 : 1;
+        }
+        this.dx = player.dx || this.dx;
     }
     if(this.x + this.radius >= width) {
-      this.dx *= -1;
+      this.dx = -1;
     }
     if(this.x - this.radius <= 0) {
       this.dx = 1;
     }
     if(this.y - this.radius <= 0) {
       this.dy = 1;
+    }
+    if(this.y >= height) {
+      if (player.hp < 1) {
+        console.log('Вы проиграли(((');
+      }
+      player.updHp(1);
+      ball.init(player.x + Math.ceil(player.width / 2), player.y - 5, 5, 'blue');
     }
   },
 };
--- player.js
+++ player.js
@@ -1,25 +1,43 @@
 var player = {
   level: 1,
-  hp: 5,
+  hp: 3,
   x: 50,
   y: 440,
   width: 100,
   height: 20,
   color: 'yellow',
   speed: 2,
-//Отрисовка игрока
+  dx: 0,
+  score: 0,
+
+  //Обновление информации о счете игрока
+  updScore: function(score) {
+    this.score += score;
+    document.getElementById('score').innerHTML = this.score;
+  },
+  //Обновление информации о колличестве оставшихся игр
+  updHp: function(hp) {
+    this.hp -= hp;
+    document.getElementById('hp').innerHTML = this.hp;
+  },
+  //Отрисовка игрока
   draw: function() {
     drawRect(this.x, this.y, this.width, this.height, this.color);
   },
-//Управление движением платформы
+  //Управление движением платформы
   move: function() {
-    if (isKeyDown('D') || isKeyDown('LEFT'))
+    if (isKeyDown('D') || isKeyDown('LEFT')) {
       this.x += this.speed;
-    if (isKeyDown('A') || isKeyDown('RIGHT'))
+      this.dx = 1;
+    } else if (isKeyDown('A') || isKeyDown('RIGHT')) {
       this.x -= this.speed;
+      this.dx = -1;
+    } else {
+      this.dx = 0;
+    }
   },
-//Инициализация игрока
-  init: function (x, w, color) {
+  //Инициализация игрока
+  init: function(x, w, color) {
     this.x = x;
     this.width = w;
     this.color = color;
